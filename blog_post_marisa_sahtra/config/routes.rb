dbaee84a1bcd992094ffa6709d1613c740ac66ed
Chawwh <chachablueftw@gmail.com>
2021-10-15 12:33:50 -0600

    Completely completed challenge AND made it pretty HAHA

---
+++ blog_post_marisa_sahtra/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+get '/blogs' => 'blog#index', as: 'blogs'
+get 'blogs/new' => 'blog#new', as: 'new_blog'
+get '/blogs/:id' => 'blog#show', as: 'blog'
+post '/blogs' => 'blog#create'
+get '/blogs/:id/edit' => 'blog#edit', as: 'edit_blog'
+patch '/blogs/:id' => 'blog#update'
+delete '/blogs/:id' => 'blog#destroy', as: 'delete_blog'
+root 'blog#index'
+end
